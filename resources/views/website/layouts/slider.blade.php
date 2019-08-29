bdba033e06570a7779dcee9751f3d9170bb2565c
Md Arif Khan <mak0099@gmail.com>
2019-08-29 23:12:10 +0300

    slider image update

--- resources/views/website/layouts/slider.blade.php
+++ resources/views/website/layouts/slider.blade.php
@@ -7,58 +7,22 @@
 							<li data-index="rs-4" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-rotate="0" data-saveperformance="off" data-title="Slide"
 											data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
 								<!-- MAIN IMAGE -->
-								<img src="{{ asset('assets/website/images/slider_2_1.jpg') }}" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
+								<img src="{{ asset('images/slider/slider1.jpg') }}" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
 								<!-- LAYERS -->
-
-								<!-- LAYER NR. 1 -->
-								<div class="tp-caption TRX-subtitle-extra   tp-resizeme  extra" id="slide-4-layer-1" data-x="right" data-hoffset="404" data-y="137" data-width="['auto']" data-height="['auto']" data-transform_idle="o:1;" data-transform_in="y:50px;opacity:0;s:1500;e:Power2.easeInOut;"
-												data-transform_out="opacity:0;s:300;" data-start="600" data-splitin="none" data-splitout="none" data-responsive_offset="on" data-captionhidden="on">Community Foundation </div>
-
-								<!-- LAYER NR. 2 -->
-								<div class="tp-caption TRX-title-extra   tp-resizeme" id="slide-4-layer-2" data-x="right" data-hoffset="20" data-y="171" data-width="['auto']" data-height="['auto']" data-transform_idle="o:1;" data-transform_in="y:50px;opacity:0;s:1400;e:Power2.easeInOut;"
-												data-transform_out="opacity:0;s:300;" data-start="1200" data-splitin="none" data-splitout="none" data-responsive_offset="on">HELP SENIOR CITIZENS </div>
-
-								<!-- LAYER NR. 3 -->
-								<div class="tp-caption TRX-button   tp-resizeme" id="slide-4-layer-3" data-x="right" data-hoffset="336" data-y="279" data-width="['auto']" data-height="['auto']" data-transform_idle="o:1;" data-transform_in="y:50px;opacity:0;s:1400;e:Power2.easeInOut;"
-												data-transform_out="opacity:0;s:300;" data-start="1700" data-splitin="none" data-splitout="none" data-responsive_offset="on"><a href="our-causes.html" class="sc_button sc_button_square sc_button_style_filled2 sc_button_size_large extra   sc_button_iconed icon-arrow">How You Can Help?</a> </div>
 							</li>
 							<!-- SLIDE  -->
 							<li data-index="rs-7" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-rotate="0" data-saveperformance="off" data-title="Slide"
 											data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
 								<!-- MAIN IMAGE -->
-								<img src="{{ asset('assets/website/images/slider_2_2.jpg') }}" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
+								<img src="{{ asset('images/slider/slider2.jpg') }}" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
 								<!-- LAYERS -->
-
-								<!-- LAYER NR. 1 -->
-								<div class="tp-caption TRX-subtitle-white   tp-resizeme  extra" id="slide-7-layer-1" data-x="170" data-y="87" data-width="['auto']" data-height="['auto']" data-transform_idle="o:1;" data-transform_in="y:50px;opacity:0;s:1500;e:Power2.easeInOut;" data-transform_out="opacity:0;s:300;"
-												data-start="600" data-splitin="none" data-splitout="none" data-responsive_offset="on" data-captionhidden="on">Community Foundation </div>
-
-								<!-- LAYER NR. 2 -->
-								<div class="tp-caption TRX-title-extra-white   tp-resizeme" id="slide-7-layer-2" data-x="170" data-y="121" data-width="['auto']" data-height="['auto']" data-transform_idle="o:1;" data-transform_in="y:50px;opacity:0;s:1400;e:Power2.easeInOut;" data-transform_out="opacity:0;s:300;"
-												data-start="1200" data-splitin="none" data-splitout="none" data-responsive_offset="on">Give a little.<br/> Change a lot. </div>
-
-								<!-- LAYER NR. 3 -->
-								<div class="tp-caption TRX-button   tp-resizeme" id="slide-7-layer-3" data-x="176" data-y="300" data-width="['auto']" data-height="['auto']" data-transform_idle="o:1;" data-transform_in="y:50px;opacity:0;s:1400;e:Power2.easeInOut;" data-transform_out="opacity:0;s:300;"
-												data-start="1700" data-splitin="none" data-splitout="none" data-responsive_offset="on"><a href="our-causes.html" class="sc_button sc_button_square sc_button_style_filled2 sc_button_size_large extra   sc_button_iconed icon-arrow">How You Can Help?</a> </div>
 							</li>
 							<!-- SLIDE  -->
 							<li data-index="rs-8" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-rotate="0" data-saveperformance="off" data-title="Slide"
 											data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
 								<!-- MAIN IMAGE -->
-								<img src="{{ asset('assets/website/images/slider_2_3.jpg') }}" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
+								<img src="{{ asset('images/slider/slider3.jpg') }}" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
 								<!-- LAYERS -->
-
-								<!-- LAYER NR. 1 -->
-								<div class="tp-caption TRX-subtitle-white   tp-resizeme  extra" id="slide-8-layer-1" data-x="right" data-hoffset="403" data-y="87" data-width="['auto']" data-height="['auto']" data-transform_idle="o:1;" data-transform_in="y:50px;opacity:0;s:1500;e:Power2.easeInOut;"
-												data-transform_out="opacity:0;s:300;" data-start="600" data-splitin="none" data-splitout="none" data-responsive_offset="on" data-captionhidden="on">Education Foundation </div>
-
-								<!-- LAYER NR. 2 -->
-								<div class="tp-caption TRX-title-extra-white   tp-resizeme" id="slide-8-layer-2" data-x="right" data-hoffset="142" data-y="121" data-width="['auto']" data-height="['auto']" data-transform_idle="o:1;" data-transform_in="y:50px;opacity:0;s:1400;e:Power2.easeInOut;"
-												data-transform_out="opacity:0;s:300;" data-start="1200" data-splitin="none" data-splitout="none" data-responsive_offset="on">help cindy<br/> back to school </div>
-
-								<!-- LAYER NR. 3 -->
-								<div class="tp-caption TRX-button   tp-resizeme" id="slide-8-layer-3" data-x="right" data-hoffset="313" data-y="300" data-width="['auto']" data-height="['auto']" data-transform_idle="o:1;" data-transform_in="y:50px;opacity:0;s:1400;e:Power2.easeInOut;"
-												data-transform_out="opacity:0;s:300;" data-start="1700" data-splitin="none" data-splitout="none" data-responsive_offset="on"><a href="our-causes.html" class="sc_button sc_button_square sc_button_style_filled2 sc_button_size_large extra   sc_button_iconed icon-arrow">How You Can Help?</a> </div>
 							</li>
 						</ul>
 						<div class="tp-bannertimer"></div>
